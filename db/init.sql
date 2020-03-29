bd3d5d4cb426b93a7922de6bfcb6deebf74d3a17
longder <longder1989@qq.com>
2020-03-29 15:06:15 +0800

    首页权限简单划分

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,9 @@
 CREATE DATABASE exam_online_sys DEFAULT CHARACTER SET UTF8;
 
 insert into SYS_USER(name_, login_name_, password_) VALUES ('管理员','admin','$2a$10$OZkHd5LbJmQcBs5NUwetL.xafO5ThfwIW3dYCwU514oVopgRmOghu');
+insert into SYS_USER(name_, login_name_, password_) VALUES ('教师1','teacher1','$2a$10$OZkHd5LbJmQcBs5NUwetL.xafO5ThfwIW3dYCwU514oVopgRmOghu');
+insert into SYS_USER(name_, login_name_, password_) VALUES ('学生1','student1','$2a$10$OZkHd5LbJmQcBs5NUwetL.xafO5ThfwIW3dYCwU514oVopgRmOghu');
+
 insert into SYS_USER_ROLE(sys_user_id_, role_) values (1,'ROLE_ADMIN');
+insert into SYS_USER_ROLE(sys_user_id_, role_) values (2,'ROLE_TEACHER');
+insert into SYS_USER_ROLE(sys_user_id_, role_) values (3,'ROLE_STUDENT');
